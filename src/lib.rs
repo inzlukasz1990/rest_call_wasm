40ab688d20e89d9b25ecd9aa2b0e71fa43e2fd71
Lukasz Fuszara <inz.lukasz.fuszara@gmail.com>
2023-04-17 21:09:52 +0200

    JSON result

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Headers};
+use js_sys::{JSON, JsString};
 
 #[wasm_bindgen]
 pub fn setup_cors(request: &Request) {
@@ -18,27 +19,27 @@ pub async fn request(
     data: Option<String>,
     token: Option<String>,
     content_type: Option<String>,
-) -> Result<JsValue, JsValue> {
+) -> Result<JsString, JsString> {
     make_request(method, url, data, token, content_type).await
 }
 
 #[wasm_bindgen]
-pub async fn get(url: &str, token: Option<String>) -> Result<JsValue, JsValue> {
+pub async fn get(url: &str, token: Option<String>) -> Result<JsString, JsString> {
     make_request("GET", url, None, token, None).await
 }
 
 #[wasm_bindgen]
-pub async fn post(url: &str, data: String, token: Option<String>, content_type: Option<String>) -> Result<JsValue, JsValue> {
+pub async fn post(url: &str, data: String, token: Option<String>, content_type: Option<String>) -> Result<JsString, JsString> {
     make_request("POST", url, Some(data), token, content_type).await
 }
 
 #[wasm_bindgen]
-pub async fn put(url: &str, data: String, token: Option<String>, content_type: Option<String>) -> Result<JsValue, JsValue> {
+pub async fn put(url: &str, data: String, token: Option<String>, content_type: Option<String>) -> Result<JsString, JsString> {
     make_request("PUT", url, Some(data), token, content_type).await
 }
 
 #[wasm_bindgen]
-pub async fn delete(url: &str, token: Option<String>) -> Result<JsValue, JsValue> {
+pub async fn delete(url: &str, token: Option<String>) -> Result<JsString, JsString> {
     make_request("DELETE", url, None, token, None).await
 }
 
@@ -48,13 +49,15 @@ async fn make_request(
     data: Option<String>,
     token: Option<String>,
     content_type: Option<String>,
-) -> Result<JsValue, JsValue> {
+) -> Result<JsString, JsString> {
     let window = web_sys::window().expect("No global `window` exists");
     let request = Request::new_with_str_and_init(url, &request_init(method, data, token, content_type)?)
         .map_err(|_| "Failed to create request")?;
     setup_cors(&request);
     let response = JsFuture::from(window.fetch_with_request(&request)).await?;
-    Ok(response)
+    let response = response.dyn_into::<web_sys::Response>()?;
+    let text = JsFuture::from(response.text()?).await?;
+    Ok(JSON::stringify(&text).unwrap())
 }
 
 fn request_init(
